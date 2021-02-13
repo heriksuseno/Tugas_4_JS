322de5bfcdbf2242cb1a3f1e43cf0643d6e1c132
heriksuseno <>
2021-02-13 19:19:38 +0700

    Tugas 4 JS untuk niomic

---
+++ tugas4.js
@@ -0,0 +1,31 @@
+var A = 189;
+var B = 169;
+var C = 175;
+
+if (A>=180) {
+  console.log("A adalah siswa paling tinggi pertama, dengan tinggi")
+} else {if (A>=170) {
+  console.log("A adalah siswa paling tinggi kedua, dengan tinggi")
+} else {
+  console.log("A adalah siswa paling tinggi ketiga, dengan tinggi")
+}
+}
+console.log(A)
+if (B>=180) {
+  console.log("B adalah siswa paling tinggi pertama, dengan tinggi")
+} else {if (B>=170) {
+  console.log("B adalah siswa paling tinggi kedua, dengan tinggi")
+} else {
+  console.log("B adalah siswa paling tinggi ketiga, dengan tinggi")
+}
+}
+console.log(B)
+if (C>=180) {
+  console.log("C adalah siswa paling tinggi pertama, dengan tinggi")
+} else {if (C>=170) {
+  console.log("C adalah siswa paling tinggi kedua, dengan tinggi")
+} else {
+  console.log("C adalah siswa paling tinggi ketiga, dengan tinggi")
+}
+}
+console.log(C)
